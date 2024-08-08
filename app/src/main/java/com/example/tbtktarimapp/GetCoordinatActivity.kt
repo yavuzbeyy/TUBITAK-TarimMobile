65ea1869dfc5a1e1a7447d91d307531e2a96560b
Yavuz <devyavuz81@gmail.com>
2024-08-08 20:05:36 +0300

    GotLocationInformations

--- app/src/main/java/com/example/tbtktarimapp/GetCoordinatActivity.kt
+++ app/src/main/java/com/example/tbtktarimapp/GetCoordinatActivity.kt
@@ -13,6 +13,16 @@ import com.google.android.gms.location.LocationServices
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import kotlin.concurrent.thread
 
 class GetCoordinatActivity : AppCompatActivity() {
 
@@ -27,6 +37,22 @@ class GetCoordinatActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // SSL sertifikalarını geçici olarak kabul etmek için TrustManager'ı ayarlama
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+            override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+        })
+
+        try {
+            val sc = SSLContext.getInstance("SSL")
+            sc.init(null, trustAllCerts, SecureRandom())
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
+            HttpsURLConnection.setDefaultHostnameVerifier { _, _ -> true }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -61,10 +87,54 @@ class GetCoordinatActivity : AppCompatActivity() {
         fusedLocationClient.lastLocation.addOnSuccessListener { location ->
             if (location != null) {
                 // API İsteği Burada yapabilirim.
+                makeApiRequest(location.latitude, location.longitude)
                 locationTextView.text = "Enlem: ${location.latitude}, Boylam: ${location.longitude}"
             } else {
                 locationTextView.text = "Konum bilgisi alınamadı."
             }
         }
     }
+
+    private fun makeApiRequest(latitude: Double, longitude: Double) {
+        thread {
+            try {
+                val url = URL("https://10.0.2.2:7107/api/Bitki/GetCityAndPlantsByLocation?enlemKoordinat=$latitude&boylamKoordinat=$longitude")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+
+                val responseCode = connection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val response = connection.inputStream.bufferedReader().use { it.readText() }
+                    val jsonResponse = JSONObject(response)
+                    val dataArray = jsonResponse.getJSONArray("data")
+
+                    val plantInfo = StringBuilder()
+                    for (i in 0 until dataArray.length()) {
+                        val plant = dataArray.getJSONObject(i)
+                        plantInfo.append("Ad: ${plant.getString("ad")}\n")
+                        plantInfo.append("Açıklama: ${plant.getString("aciklama")}\n")
+                        plantInfo.append("İklim: ${plant.getString("iklimAdi")}\n")
+                        plantInfo.append("Toprak: ${plant.getString("toprakAdi")}\n")
+                        plantInfo.append("Sulama: ${plant.getString("sulamaAdi")}\n")
+                        plantInfo.append("Gübreleme: ${plant.getString("gubrelemeAdi")}\n\n")
+                    }
+
+                    runOnUiThread {
+                        locationTextView.text = plantInfo.toString()
+                    }
+                } else {
+                    runOnUiThread {
+                        locationTextView.text = "API isteği başarısız: $responseCode"
+                    }
+                }
+                connection.disconnect()
+            } catch (e: Exception) {
+                runOnUiThread {
+                    locationTextView.text = "Hata: ${e.message}"
+                }
+            }
+        }
+    }
+
+
 }
